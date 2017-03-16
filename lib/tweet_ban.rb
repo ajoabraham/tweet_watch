f9f59b90512ee33c51f8956e5b085584f075a3f8
Ajo Abraham <abraham.ajo@gmail.com>
2017-03-16 15:36:16 -0400

    follower

--- lib/tweet_ban.rb
+++ lib/tweet_ban.rb
@@ -1,5 +1,15 @@
 require "tweet_ban/version"
+require "tweet_ban/timeline"
+require "twitter"
 
 module TweetBan
-  # Your code goes here...
+  
+  @config = {
+    consumer_key: "jKgFRB9dogVmyRB1P4Wm5J7CN",
+    consumer_secret: "oMswt4MMBJxrtfQ27JkmbuLfHMvkZKrlSpIcT8AdmubJcSjlpT"
+  }
+  
+  def self.run
+    
+  end
 end
---
+++ lib/tweet_ban/follower.rb
@@ -0,0 +1,7 @@
+module TweetBan
+  
+  class Follower
+    attr_accessor :screen_name, :access_token, :access_token_secret
+  end
+  
+end
