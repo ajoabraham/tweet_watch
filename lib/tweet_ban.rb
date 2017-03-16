b4dd5a5b2a7d8ff8bfc722d8e38611c6ef8864ae
Ajo Abraham <abraham.ajo@gmail.com>
2017-03-16 15:44:57 -0400

    more

--- lib/tweet_ban.rb
+++ lib/tweet_ban.rb
@@ -1,5 +1,5 @@
 require "tweet_ban/version"
-require "tweet_ban/timeline"
+require "tweet_ban/follower"
 require "twitter"
 
 module TweetBan
@@ -10,6 +10,20 @@ module TweetBan
   }
   
   def self.run
+    f = Follwer.new("ajoabraham", 
+      "17609705-Fd9WcmbEEFFftEwxxFvF1FOgtZ1S4E1YNnj0XcPyc",
+      "VIge9jyWlz9k9qe0jVK3gHoSwqMlgsi0cRZwZEYpWXu6G")
+      
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = @config.consumer_key
+      config.consumer_secret     = @config.consumer_secret
+      config.access_token        = f.access_token
+      config.access_token_secret = f.access_secret_token
+    end
+    
+    client.home_timeline.each do |ht|
+      puts ht
+    end
     
   end
 end
--- lib/tweet_ban/follower.rb
+++ lib/tweet_ban/follower.rb
@@ -2,6 +2,13 @@ module TweetBan
   
   class Follower
     attr_accessor :screen_name, :access_token, :access_token_secret
+    
+    def initialize(screen_name, token, secret)
+      @screen_name = screen_name
+      @access_token = token
+      @acess_token_secret = secret      
+    end
+    
   end
   
 end
